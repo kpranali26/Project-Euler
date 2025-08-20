c7367baca375714356000d2de788e3047d2e5f27
kpranali26 <katharpranali26@gmail.com>
2025-08-20 23:34:09 +0530

    .

--- PowerSum_16.java
+++ PowerSum_16.java
@@ -1,25 +1,46 @@
-public class PowerSum_16 {
+// public class PowerSum_16 {
     
-    public static void main(String[] args) {
-        int i = 2;
-        int product = 1;
-        int count = 1;
-        while ( count != 11){
-          product *= i;
-          count++;
-        }
-        int val = product;
-        product *= val;
-        product *= val;
+//     public static void main(String[] args) {
+//         int i = 2;
+//         int product = 1;
+//         int count = 1;
+//         while ( count != 11){
+//           product *= i;
+//           count++;
+//         }
+//         int val = product;
+//         product *= val;
+//         product *= val;
         
-        System.out.println("The product is :" + product);
-       int s = 0;
-        while (product > 0){
-            s += product % 10;
-            product = product/10;
-        }
+//         System.out.println("The product is :" + product);
+//        int s = 0;
+//         while (product > 0){
+//             s += product % 10;
+//             product = product/10;
+//         }
 
-      System.out.println(s);
+//       System.out.println(s);
         
+//     }
+// }
+
+
+
+import java.math.BigInteger;
+public class PowerSum_16 {
+  public static void main(String[] args) {
+    BigInteger num = BigInteger.valueOf(2).pow(1000);
+
+    String s = num.toString();
+     
+   int sum = 0;
+    for(char c : s.toCharArray()){
+
+      sum+= c -'0';
+
+
     }
+    System.out.println("2 ki 1000 =" + num);
+    System.out.println("Sum of all digits :" + sum);
+  }
 }
--- latticePath_15.java
+++ latticePath_15.java
@@ -1,7 +1,19 @@
 public class latticePath_15 {
-    
-    private static int helper(int count , int row , int col){
-        
+    public static void main(String[] args) {
+        int num = helper(2, 2);
+        System.out.println("Total routes are :" + num);
     }
-
+    private static int helper(int row, int col){
+        if(row == 1 || col == 1){
+            return 1;
+        }
+        int count = 0;
+        if(row > 0){
+            count += helper(row - 1, col);
+        }
+        if(col > 0){
+            count += helper(row, col - 1);
+        }
+        return count;
+    }
 }
