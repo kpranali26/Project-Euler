823e114ea77b5e1d9247c02f2d5e567457362bb8
kpranali26 <katharpranali26@gmail.com>
2025-08-23 23:47:44 +0530

    .

--- AmicableNumber_21.java
+++ AmicableNumber_21.java
@@ -1,23 +1,26 @@
-
-import java.util.HashSet;
 public class AmicableNumber_21 {
-    static HashSet<Integer> set = new HashSet<>();
     public static void main(String[] args) {
-        int numDiv = sumOfdivisor(220);
-        System.out.println(numDiv);
+        int numDiv = sumOfDivisors(220);
+        System.out.println("Sum of proper divisors of 220 = " + numDiv);
+
+        int a = 220, b = numDiv;
+        if (a != b && sumOfDivisors(b) == a) {
+            System.out.println(a + " and " + b + " are amicable numbers.");
+        } else {
+            System.out.println("Not amicable.");
+        }
     }
 
-    private static int sumOfdivisor(int num){
-       int sum = 0;
-       for(int i = 1; i*i <= num ; i++ ){
-        if(num % i == 0){
-        set.add(num);
-        set.add(i);
+    private static int sumOfDivisors(int num) {
+        int sum = 1; // 1 is always a divisor
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i != num / i) { // avoid double-counting square root
+                    sum += num / i;
+                }
+            }
         }
-       }
-       for (int val : set) {
-        sum += val;
-       }
-       return sum;
+        return sum;
     }
 }
